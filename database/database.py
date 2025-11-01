c88bfe885d264ccebe31f92cc205cc3fd1084c2e
CarolAtolini <carolineatolini@gmail.com>
2025-11-01 20:27:06 -0300

    Create database.py

---
+++ database/database.py
@@ -0,0 +1,28 @@
+import pymongo
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+DATABASE_URL = os.environ.get("DATABASE_URL", "").split(",")
+DEVELOPMENT = os.environ.get("DEVELOPMENT", "false")
+if(DEVELOPMENT == "true"):
+    DEVELOPMENT = True
+else:
+    DEVELOPMENT = False
+
+client = pymongo.MongoClient(
+    host=DATABASE_URL,
+    tls=True,
+    tlsAllowInvalidCertificates=DEVELOPMENT,
+    retryWrites=False,
+)
+
+print("Conectando com CosmoDB...")
+if(client.is_mongos):
+    print("Conectado com CosmoDB")
+
+db = client.get_database("synapse")
+
+chats = db["chats"]
+users = db["users"]
